c557b043754108ed88d9196cefbd6b02f5784a23
takatakasan-noid <iamtakataka321@gmail.com>
2026-08-24 20:29:05 +0900

    OAuthクライアントIDを更新

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Google Cloud Console で取得した値をここに入れてください。
 const CONFIG = {
   // OAuth 2.0 クライアントID（「APIとサービス」→「認証情報」で作成したもの）
-  CLIENT_ID: "YOUR_CLIENT_ID.apps.googleusercontent.com",
+  CLIENT_ID: "1086462299086-ja81njbib8al7mos58a32lbfp2ss1id5.apps.googleusercontent.com",
 
   // 音源を入れるGoogle DriveフォルダのID
   // フォルダURL: https://drive.google.com/drive/folders/【この部分】
